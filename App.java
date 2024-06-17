6a3590d895135e2e1f44657f2a6704f08425aee3
aidenrk <86091529+aidenrk@users.noreply.github.com>
2024-06-17 00:24:41 -0400

    Critical Thinking 1

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /// Declaring fake variables to be printed during runtime
+        String fakeFirstName = "\nAidenn";
+        String fakeLastName = "Kyzerr";
+        String fakeAddress = "800 keats drr";
+        String fakeCity = "Spartanburg";
+        String fakeZip = "293011";
+
+        /// Printout out the information statically assigned above
+        System.out.println(fakeFirstName);
+        System.out.println(fakeLastName);
+        System.out.println(fakeAddress);
+        System.out.println(fakeCity);
+        System.out.println(fakeZip);
+    }
+}
